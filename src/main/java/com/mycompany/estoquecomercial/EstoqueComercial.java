74722c3996088fe527370793e05356b6457ebb40
K <116846922+dev-kaio@users.noreply.github.com>
2026-04-06 19:12:07 -0300

    Limpando comentários em EstoqueComercial.java

--- src/main/java/com/mycompany/estoquecomercial/EstoqueComercial.java
+++ src/main/java/com/mycompany/estoquecomercial/EstoqueComercial.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.estoquecomercial;
 
-/**
- *
- * @author k
- */
 public class EstoqueComercial {
 
     public static void main(String[] args) {
